d649fc9a7f816ad4ee2b1200ab8e9c9034848ff3
russl8 <russl8@my.yorku.ca>
2025-03-11 16:58:57 -0400

    chore: playing around with designs

--- components/displays/BookmarkManager.tsx
+++ components/displays/BookmarkManager.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
 const BookmarkManager = () => {
-    return (  
-        <></>
+    return (
+        <>
+            <Bookmark />
+        </>
     );
 }
- 
+
+const Bookmark = () => {
+    return (
+        <div className="flex flex-col justify-center h-20 w-14 bg-gra">
+
+            <div className="object-contain overflow-hidden h-14 w-14 rounded-sm">
+                <Image alt="sample" src="/squareImage.jpg" width={4000} height={4000} />
+            </div>
+            <p className=" text-center text-sm">EClass</p>
+        </div>
+    )
+}
 export default BookmarkManager;
