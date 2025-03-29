e3c12e3d715ee7276ef751fd5df7b938811392bc
russl8 <russl8@my.yorku.ca>
2025-03-29 15:50:01 -0400

    refactor: nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,14 @@
-// import type { NextConfig } from "next";
-
-// module.exports = {
- 
-// };
-
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   images: {
     domains: ['untitled-bookmarks.s3.us-east-2.amazonaws.com'],
   },
-  webpack(config: any) {
-    config.resolve.alias["@"] = path.join(__dirname); 
+  webpack: (config:any) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'), // This points to the root directory of your project
+    };
     return config;
   },
 };
-
