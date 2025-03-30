4632085a23711e57f8a90f286f48f091c45a8e50
russl8 <russl8@my.yorku.ca>
2025-03-29 22:28:06 -0400

    refactor: filepaths

---
+++ model/bookmark.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const BookmarkSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    bookmarkName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    bookmarkImage: {
+      type: String,
+      required: true,
+    },
+    s3FileKey: {
+      type: String,
+      required: true,
+    },
+    bookmarkLink: {
+      type: String,
+      required: true,
+      match: [/^https?:\/\/.*/, "Invalid URL format"],
+    },
+  },
+  { timestamps: true }
+);
+
+const Bookmark =
+  mongoose.models.Bookmark || mongoose.model("Bookmark", BookmarkSchema);
+
+export default Bookmark;
