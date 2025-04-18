05c7d56cd5afb69d3934463926ab1d55c3445b63
russl8 <russl8@my.yorku.ca>
2025-04-17 23:31:13 -0400

    fix: button aschild

--- components/workoutTracker/dashboard/WorkoutCard.tsx
+++ components/workoutTracker/dashboard/WorkoutCard.tsx
@@ -36,39 +36,40 @@ const WorkoutCard = ({ workout, fetchWorkouts }: WorkoutCardProps) => {
             <TooltipProvider>
                 <Tooltip>
 
-                    <TooltipTrigger>
-                        <DialogTrigger id="workoutDetailsModalTriggerClick">
+                    <DialogTrigger id="workoutDetailsModalTriggerClick">
+                        <TooltipTrigger asChild>
 
-                            <div
-                                className={cn(
-                                    `group hover:scale-105 bg-white text-black rounded-2xl shadow-md hover:shadow-xl transition-all duration-300 cursor-pointer 
+                        <div
+                            className={cn(
+                                `group hover:scale-105 bg-white text-black rounded-2xl shadow-md hover:shadow-xl transition-all duration-300 cursor-pointer 
                                  overflow-hidden select-none px-4 py-3 m-2 flex flex-col justify-between w-[160px] h-[220px] text-sm`,
 
-                                )}
-                            >
-                                <div className="flex text-sm justify-between items-center font-semibold text-gray-800 mb-2">
-                                    <p className="truncate w-[70%]">
-                                        {workout.workoutName.slice(0, TITLE_TRIM_AMOUNT)}
-                                        {workout.workoutName.length > TITLE_TRIM_AMOUNT && "..."}
-                                    </p>
-                                    <span className="text-xs text-gray-500">{getmmdd(workout.lastUpdated)}</span>
-                                </div>
-
-                                <div className="flex-1 overflow-y-scroll">
-                                    {workout.exercises.map((exercise, index) => (
-                                        <div key={index} className="flex justify-between items-center text-gray-700 py-[1px]">
-                                            <span className="truncate">
-                                                {exercise.exerciseName.slice(0, 11)}
-                                                {exercise.exerciseName.length > 11 && "..."}
-                                            </span>
-                                            <span className="font-semibold">{exercise.reps}x{exercise.sets}</span>
-                                        </div>
-                                    ))}
-                                </div>
+                            )}
+                        >
+                            <div className="flex text-sm justify-between items-center font-semibold text-gray-800 mb-2">
+                                <p className="truncate w-[70%]">
+                                    {workout.workoutName.slice(0, TITLE_TRIM_AMOUNT)}
+                                    {workout.workoutName.length > TITLE_TRIM_AMOUNT && "..."}
+                                </p>
+                                <span className="text-xs text-gray-500">{getmmdd(workout.lastUpdated)}</span>
                             </div>
-                        </DialogTrigger>
 
-                    </TooltipTrigger>
+                            <div className="flex-1 overflow-y-scroll">
+                                {workout.exercises.map((exercise, index) => (
+                                    <div key={index} className="flex justify-between items-center text-gray-700 py-[1px]">
+                                        <span className="truncate">
+                                            {exercise.exerciseName.slice(0, 11)}
+                                            {exercise.exerciseName.length > 11 && "..."}
+                                        </span>
+                                        <span className="font-semibold">{exercise.reps}x{exercise.sets}</span>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                        </TooltipTrigger>
+
+                    </DialogTrigger>
+
 
                     <TooltipContent sideOffset={-10} className="p-0">
                         <div className="flex flex-col text-sm">
